1bdc8763355f551d5230d70b9cea570730016625
effervescentia <ben.l.teichman@gmail.com>
2014-06-04 16:09:19 -0400

    Added vm deploy to main deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-while getopts 'lr' opt; do
+while getopts 'lrv' opt; do
   case $opt in
     l)
       HOST='rachel_local';
@@ -6,13 +6,18 @@ while getopts 'lr' opt; do
     r)
       HOST='rachel_remote';
       ;;
-    *)
-
+    v)
+      status=`vagrant status | grep test | grep running`;
+      if [[ -z $status ]]; then
+        vagrant up test
+      fi
+      HOST='vlocal';
+      ;;
   esac
 done
 
 if [[ -z $HOST ]]; then
-  echo -e 'ERROR:\tmust specify either local (-l) or remote (-r)';
+  echo -e 'ERROR:\tmust specify either local (-l), remote (-r) or virtual (-v)';
 else
   scp -r app/* $HOST:/var/www
 fi
