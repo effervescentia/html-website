b4b0372a7463243b8774ab8b8db82225851b1312
effervescentia <ben.l.teichman@gmail.com>
2014-06-04 01:51:15 -0400

    Edited deploy script to require flag for local or remote use

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,18 @@
-scp -r app/* rachel:/var/www
+while getopts 'lr' opt; do
+  case $opt in
+    l)
+      HOST='rachel_local';
+      ;;
+    r)
+      HOST='rachel_remote';
+      ;;
+    *)
+
+  esac
+done
+
+if [[ -z $HOST ]]; then
+  echo -e 'ERROR:\tmust specify either local (-l) or remote (-r)';
+else
+  scp -r app/* $HOST:/var/www
+fi
