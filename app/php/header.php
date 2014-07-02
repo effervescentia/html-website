dbfe010446b1554bc9d94465c269d2cf25c1ce9e
effervescentia <ben.l.teichman@gmail.com>
2014-07-01 22:59:15 -0400

    Links no longer end in .php

--- app/php/header.php
+++ app/php/header.php
@@ -2,10 +2,10 @@
   <div class="container">
     <ul class="pull-right">
       <li><a href="/">home</a></li>
-      <li><a href="projects.php">projects</a></li>
-      <li><a href="experience.php">experience</a></li>
-      <li><a href="about.php">about</a></li>
-      <li><a href="contact.php">contact</a></li>
+      <li><a href="projects">projects</a></li>
+      <li><a href="experience">experience</a></li>
+      <li><a href="about">about</a></li>
+      <li><a href="contact">contact</a></li>
     </ul>
   </div>
 </div>
